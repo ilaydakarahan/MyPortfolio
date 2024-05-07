77943049a932f1ea657f4503581cff09b8e644ea
İlayda Karahan <ilaydapala15@gmail.com>
2024-05-07 23:55:09 +0300

    Add project files.

---
+++ MyPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        MyPortfolioProjectEntities db=new MyPortfolioProjectEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblAbouts.ToList();
+            return View(values);
+        }
+    }
+}
